e2330415c53dffcd4d64f71a43c877ae39c7c435
Arjun Mahishi <arjun@exotel.in>
2019-11-06 14:19:45 +0530

    un-gitignoring the sync script

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# clean 
+find . -maxdepth 1 -name ".*" | grep -v git | xargs sudo rm -rf
+
+# copy
+sudo cp ~/.vimrc .
+sudo cp ~/.zshrc .
+sudo cp ~/.zsh_aliases .
+git commit -am "`date|awk '{print $NF"-"$2"-"$3,$4}'`"
+git push origin master
