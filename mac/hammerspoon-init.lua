72667b9218525999dbbd0e8835d150cc29801eed
Arjun Mahishi <arjun.mahishi@gmail.com>
2021-08-28 11:07:53 +0530

    [personal-mbp] Sat Aug 28 11:07:53 IST 2021

--- mac/hammerspoon-init.lua
+++ mac/hammerspoon-init.lua
@@ -6,5 +6,5 @@ end
 
 hs.hotkey.bind({"cmd"}, "1", open_app("iterm"))
 hs.hotkey.bind({"cmd"}, "2", open_app("brave browser"))
-hs.hotkey.bind({"cmd"}, "3", open_app("slack"))
-hs.hotkey.bind({"cmd"}, "4", open_app("spotify"))
+-- hs.hotkey.bind({"cmd"}, "3", open_app("slack"))
+hs.hotkey.bind({"cmd"}, "3", open_app("spotify"))
