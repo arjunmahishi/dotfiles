2de52a76ef9406f1382776c34bd9ae0c61e050a2
Arjun Mahishi <arjun.mahishi@gmail.com>
2021-10-31 19:28:34 +0530

    [mac] remap cmd + 1 to iterm

--- mac/hammerspoon-init.lua
+++ mac/hammerspoon-init.lua
@@ -4,7 +4,7 @@ function open_app(name)
     end
 end
 
-hs.hotkey.bind({"cmd"}, "1", open_app("kitty"))
+hs.hotkey.bind({"cmd"}, "1", open_app("iterm"))
 hs.hotkey.bind({"cmd"}, "2", open_app("brave browser"))
 -- hs.hotkey.bind({"cmd"}, "3", open_app("slack"))
 hs.hotkey.bind({"cmd"}, "3", open_app("spotify"))
