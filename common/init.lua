99a7b5bbf15d8cbd2ab4338cffedb95bebaff4e8
Arjun Mahishi <arjun.mahishi@gmail.com>
2021-11-03 00:28:14 +0530

    [linux] vim -- enable tresitter

--- common/init.lua
+++ common/init.lua
@@ -361,10 +361,10 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
--- require('nvim-treesitter.parsers').get_parser_configs().norg = {
---     install_info = {
---         url = "https://github.com/nvim-neorg/tree-sitter-norg",
---         files = { "src/parser.c", "src/scanner.cc" },
---         branch = "main"
---     },
--- }
+require('nvim-treesitter.parsers').get_parser_configs().norg = {
+  install_info = {
+    url = "https://github.com/nvim-neorg/tree-sitter-norg",
+    files = { "src/parser.c", "src/scanner.cc" },
+    branch = "main"
+  },
+}
