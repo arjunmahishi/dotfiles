7eef2f531c481935e29584056a19c34d0fe6dce4
Arjun Mahishi <arjun.mahishi@gmail.com>
2022-03-04 20:49:30 +0530

    [mac] Fri Mar  4 20:49:30 IST 2022

--- mac/hammerspoon-init.lua
+++ mac/hammerspoon-init.lua
@@ -1,20 +1,20 @@
 -- TODO: figure out how to dynamically assign hotkeys based on what applications are open
 
-function open_app(name) 
+local function open_app(name)
   hs.application.launchOrFocus(name)
 end
 
-function open_primary_app()
-  return function()
-    app = "kitty"
-    if hs.application.find("Live") then
-        app = "Live"
-    end
-    open_app(app)
-  end
-end
+-- local function open_primary_app()
+--   return function()
+--     app = "kitty"
+--     if hs.application.find("Live") then
+--         app = "Live"
+--     end
+--     open_app(app)
+--   end
+-- end
 
-function open_other_apps(name)
+local function open_other_apps(name)
   return function() open_app(name) end
 end
 
