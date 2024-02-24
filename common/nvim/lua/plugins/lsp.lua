6c8db08f854fa4830116a4d14ab7a67f35ce7fde
Arjun Mahishi <arjun.mahishi@gmail.com>
2024-02-24 16:23:57 +0530

    fix gopls flakiness to an extent

--- common/nvim/lua/plugins/lsp.lua
+++ common/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 SERVERS = { "gopls", "lua_ls", "jedi_language_server" }
 
-local function lsp_setup()
+local function lsp_hover_handler()
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     vim.lsp.handlers.hover, {
       border = "single",
@@ -9,15 +9,39 @@ local function lsp_setup()
   )
 end
 
+local function lsp_on_attach()
+  vim.diagnostic.config({
+    update_in_insert = true, -- this fixes the gopls issue where it has outdated diagnostics
+    virtual_text = {
+      prefix = '●',
+    }
+  })
+end
+
 local function lsp_config()
   local lspconfig = require('lspconfig')
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
 
   for _, server in ipairs(SERVERS) do
-    lspconfig[server].setup {
+    -- baseline LSP config. Applies to all servers
+    local config = {
       capabilities = capabilities,
+      on_attach = lsp_on_attach,
     }
+
+    -- lua override
+    if server == "lua_ls" then
+      config.settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      }
+    end
+
+    lspconfig[server].setup(config)
   end
 end
 
@@ -43,7 +67,7 @@ return {
     -- LSP configuration
     {
         "neovim/nvim-lspconfig",
-        init = lsp_setup,
+        init = lsp_hover_handler,
         config = lsp_config,
         keys = {
             { "gd", "<cmd>lua vim.lsp.buf.definition()<CR>" },
