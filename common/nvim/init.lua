23c7263f8fda8f693080adf6f3c263bfe8093dff
Arjun Mahishi <arjun.mahishi@gmail.com>
2025-06-09 08:30:43 +0530

    update styles and colorscheme
    - Updated tmux configuration to use Gruvbox-inspired colors.
    - Replaced Tokyo Night colorscheme with Gruvbox Material in Neovim.
    - Adjusted Neovim keybindings and helper functions for better readability.
    - Removed Tokyo Night from lazy-lock.json and added Gruvbox Material.

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 ------------------------------------
 --       Setup lazy.nvim
@@ -7,74 +7,73 @@ vim.g.mapleader = ' '
 local username = string.gsub(vim.fn.system("whoami"), "\n", "")
 local lazypath = string.format("/Users/%s/.nvim-plugins/lazy.nvim", username)
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  root = string.format("/Users/%s/.nvim-plugins", username),
-  change_detection = {
-    notify = false,
-  },
+	root = string.format("/Users/%s/.nvim-plugins", username),
+	change_detection = {
+		notify = false,
+	},
 })
 
 ------------------------------------
 --        Custome key bindings
 ------------------------------------
 
-
 local map = vim.api.nvim_set_keymap
 local noremap = { noremap = true }
 
-map('i', 'jj', '<Esc>', {})
-map('n', 'vv', ':vsplit<CR>', {})
-map('n', 'tt', ':tabnew<CR>', {})
-map('n', '<C-s>', ':source ~/.config/nvim/init.lua<CR>', {})
-map('v', '<leader>y', '"+y', {})
-map('n', '<leader>p', '"+p', {})
-map('n', '<C-l>', ':nohlsearch<cr>', {})
-map('n', '<leader>fd', ':lua OpenFilesInRepo()<cr>', {})
+map("i", "jj", "<Esc>", {})
+map("n", "vv", ":vsplit<CR>", {})
+map("n", "tt", ":tabnew<CR>", {})
+map("n", "<C-s>", ":source ~/.config/nvim/init.lua<CR>", {})
+map("v", "<leader>y", '"+y', {})
+map("n", "<leader>p", '"+p', {})
+map("n", "<C-l>", ":nohlsearch<cr>", {})
+map("n", "<leader>fd", ":lua OpenFilesInRepo()<cr>", {})
 
 -- since space is used as the supream leader, make sure that is doesn't do anything
 -- else. Because no one should have that much power
-map('n', '<SPACE>', '<Nop>', noremap)
-map('v', '<SPACE>', '<Nop>', noremap)
+map("n", "<SPACE>", "<Nop>", noremap)
+map("v", "<SPACE>", "<Nop>", noremap)
 
 -- switching between panes
-map('n', '<leader>w', '<c-w><c-w>', noremap)
-map('n', '<leader>h', '<c-w>h', noremap)
-map('n', '<leader>j', '<c-w>j', noremap)
-map('n', '<leader>k', '<c-w>k', noremap)
-map('n', '<leader>l', '<c-w>l', noremap)
+map("n", "<leader>w", "<c-w><c-w>", noremap)
+map("n", "<leader>h", "<c-w>h", noremap)
+map("n", "<leader>j", "<c-w>j", noremap)
+map("n", "<leader>k", "<c-w>k", noremap)
+map("n", "<leader>l", "<c-w>l", noremap)
 
 -- BarBar
-map('n', '<C-j>', ':BufferPrevious<CR>', {})
-map('n', '<C-k>', ':BufferNext<CR>', {})
-map('n', '<C-x>', ':BufferClose<CR>', {})
+map("n", "<C-j>", ":BufferPrevious<CR>", {})
+map("n", "<C-k>", ":BufferNext<CR>", {})
+map("n", "<C-x>", ":BufferClose<CR>", {})
 
 -- terminal
-map('t', '<Esc>', '<C-\\><C-n>', noremap)
+map("t", "<Esc>", "<C-\\><C-n>", noremap)
 
 -- float term
-map('n', 'gt', ':FloatermToggle<cr>', noremap)
+map("n", "gt", ":FloatermToggle<cr>", noremap)
 
 -- formatting
-vim.cmd [[
+vim.cmd([[
   au filetype json nmap <leader>f :%!jq '.' %<CR>
   au filetype hcl nmap <leader>f :%!hclfmt %<CR>
-]]
+]])
 
 -- quickfix
-map('n', '<leader>q', ':lua vim.lsp.diagnostic.set_loclist()<CR>', {})
-map('n', '<leader>]', ':cnext<cr>', noremap)
-map('n', '<leader>[', ':cprevious<cr>', noremap)
+map("n", "<leader>q", ":lua vim.lsp.diagnostic.set_loclist()<CR>", {})
+map("n", "<leader>]", ":cnext<cr>", noremap)
+map("n", "<leader>[", ":cprevious<cr>", noremap)
 
 vim.cmd([[
   augroup GoAutocmds
@@ -85,66 +84,65 @@ vim.cmd([[
 ]])
 
 -- Gdiff
-map('n', 'g2', ':diffget //2 | diffupdate <CR>', {})
-map('n', 'g3', ':diffget //3 | diffupdate <CR>', {})
+map("n", "g2", ":diffget //2 | diffupdate <CR>", {})
+map("n", "g3", ":diffget //3 | diffupdate <CR>", {})
 
 ------------------------------------
 --        Helper functions
 ------------------------------------
 
 local function replace_vars(str, vars)
-  for k, v in pairs(vars) do
-    str = string.gsub(str, string.format("${%s}", k), v)
-  end
-  return str
+	for k, v in pairs(vars) do
+		str = string.gsub(str, string.format("${%s}", k), v)
+	end
+	return str
 end
 
 function StatusLine()
-  local git_branch = vim.fn.system('git branch --show-current 2>/dev/null')
-  git_branch = string.gsub(git_branch, "\n", "")
+	local git_branch = vim.fn.system("git branch --show-current 2>/dev/null")
+	git_branch = string.gsub(git_branch, "\n", "")
 
-  if git_branch ~= "" then
-    git_branch = string.format(" %s · ", git_branch)
-  end
+	if git_branch ~= "" then
+		git_branch = string.format(" %s · ", git_branch)
+	end
 
-  vim.g.modes = {
-    ["n"] = "NORMAL",
-    ["v"] = "VISUAL",
-    ["V"] = "V-LINE",
-    [""] = "V-BLOCK",
-    ["i"] = "INSERT",
-    ["c"] = "COMMAND",
-    ["t"] = "TERMINAL",
-  }
+	vim.g.modes = {
+		["n"] = "NORMAL",
+		["v"] = "VISUAL",
+		["V"] = "V-LINE",
+		[""] = "V-BLOCK",
+		["i"] = "INSERT",
+		["c"] = "COMMAND",
+		["t"] = "TERMINAL",
+	}
 
-  -- Check if LSP is active
-  -- local lsp_indicator = ""
-  -- local clients = vim.lsp.get_active_clients({ bufnr = 0 })
-  -- if #clients > 0 then
-  --   lsp_indicator = "LSP ✔"
-  -- else
-  --   lsp_indicator = "LSP ✖"
-  -- end
+	-- Check if LSP is active
+	-- local lsp_indicator = ""
+	-- local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+	-- if #clients > 0 then
+	--   lsp_indicator = "LSP ✔"
+	-- else
+	--   lsp_indicator = "LSP ✖"
+	-- end
 
-  -- return replace_vars(
-  --   "%{get(g:modes, mode())} · %f %r %m · ${lsp_indicator} %=%l:%c · %p%% · ${git_branch}%{&filetype} ", {
-  --     git_branch = git_branch,
-  --     lsp_indicator = lsp_indicator,
-  --   })
+	-- return replace_vars(
+	--   "%{get(g:modes, mode())} · %f %r %m · ${lsp_indicator} %=%l:%c · %p%% · ${git_branch}%{&filetype} ", {
+	--     git_branch = git_branch,
+	--     lsp_indicator = lsp_indicator,
+	--   })
 
-  return replace_vars(
-    "%{get(g:modes, mode())} · %f %r %m %=%l:%c · %p%% · ${git_branch}%{&filetype} ", {
-      git_branch = git_branch,
-    })
+	return replace_vars("%{get(g:modes, mode())} · %f %r %m %=%l:%c · %p%% · ${git_branch}%{&filetype} ", {
+		git_branch = git_branch,
+	})
 end
 
 function TelescopeIntoDir(dir)
-  require('telescope.builtin').find_files(require('telescope.themes').get_ivy({ search_dirs = { dir } }))
+	require("telescope.builtin").find_files(require("telescope.themes").get_ivy({ search_dirs = { dir } }))
 end
 
 function OpenFilesInRepo()
-  local root = vim.fn.finddir('.git/..', ';')
-  TelescopeIntoDir(root)
+	local root = vim.fn.finddir(".git/..", ";")
+	TelescopeIntoDir(root)
 end
 
 ------------------------------------
@@ -160,26 +158,24 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.incsearch = true
 vim.opt.smartcase = true
-vim.opt.colorcolumn = '121'
+vim.opt.colorcolumn = "121"
 vim.opt.lazyredraw = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.mouse = 'a'
-vim.opt.encoding = 'utf8'
-vim.opt.guifont = 'Fira Code Nerd'
+vim.opt.mouse = "a"
+vim.opt.encoding = "utf8"
+vim.opt.guifont = "Fira Code Nerd"
 vim.opt.showmode = false
 vim.opt.scrolloff = 10
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 vim.opt.wrap = false
-vim.opt.dictionary = '/usr/share/dict/words'
-vim.opt.signcolumn = 'yes:1'
+vim.opt.dictionary = "/usr/share/dict/words"
+vim.opt.signcolumn = "yes:1"
 vim.opt.statusline = StatusLine()
 
-vim.cmd [[
+vim.cmd([[
   au TextYankPost * silent! lua vim.highlight.on_yank()
-]]
-
-vim.cmd [[colorscheme tokyonight-night]]
+]])
 
 -- make backgroud transparent
-vim.cmd [[ hi Normal guibg=none ]]
+vim.cmd([[ hi Normal guibg=none ]])
--- common/nvim/lua/plugins/colorscheme.lua
+++ common/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,31 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    opts = function()
-      on_colors = function(colors)
-        colors.gitSigns = {
-          add = colors.green,
-          change = colors.orange,
-          delete = colors.red,
-          conflict = '#e5c07b',
-        }
-      end
-    end
-  },
-  { "tjdevries/colorbuddy.nvim" },
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	opts = function()
+	-- 		on_colors = function(colors)
+	-- 			colors.gitSigns = {
+	-- 				add = colors.green,
+	-- 				change = colors.orange,
+	-- 				delete = colors.red,
+	-- 				conflict = "#e5c07b",
+	-- 			}
+	-- 		end
+	-- 	end,
+	-- },
+	{ "tjdevries/colorbuddy.nvim" },
+	{
+		"sainnhe/gruvbox-material",
+		enabled = true,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_transparent_background = 1
+			vim.g.gruvbox_material_foreground = "mix"
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_ui_contrast = "high"
+			vim.g.gruvbox_material_float_style = "bright"
+			vim.g.gruvbox_material_statusline_style = "material"
+			vim.g.gruvbox_material_cursor = "auto"
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
 }
